5e5715cdb55b958395c701b8a792118a85e2ee26
Nick Stogner <nicholas.stogner@gmail.com>
2015-08-21 08:01:16 -0400

    Switched from pathogen to vundle for package management

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+function title {
+  printf "\n${BLUE}"
+  echo $1
+  printf "${NC}"
+}
+
+title "Installing vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+title "Linking .vimrc"
+cp ~/.vimrc ~/.vimrc.old
+unlink ~/.vimrc
+ln -s ~/.vim/.vimrc ~/.vimrc
+
+title "Installing vim plugins"
+vim +PluginInstall +qall
+
+title "Installing jshint via npm"
+sudo npm install -g jshint
